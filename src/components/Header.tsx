b1125e510e8a329ff2dfdaa7a4de3f586fa7364e
Adao-Angelo <adaobegginer@gmail.com>
2024-10-11 15:09:47 +0100

    update radme file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ import NewPostButton from "./newPostButton";
 
 export default async function Header() {
   return (
-    <header className="flex fixed w-[100%] bg-background z-50 border-b-[1px] items-center justify-between border-gray-400 px-16 ">
+    <header className="flex fixed w-[100%] bg-background z-50 border-b-[1px] items-center justify-between border-input px-16 ">
       <div className="flex gap-4 p-2 items-center">
         <Popover>
           <Avatar>
