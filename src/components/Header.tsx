325bd0f3b940aec48c8127b05e6bd12f81e15030
Adao-Angelo <adaobegginer@gmail.com>
2024-10-28 15:11:12 +0100

    create layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,8 @@ import Profile from "./Profile";
 
 export default async function Header() {
   return (
-    <header className="flex fixed w-[100%] bg-background z-50 border-b-[1px] items-center justify-between border-input px-4 sm:px-8 md:px-16">
-      <div className="flex gap-4 p-2 items-center">
-        <Profile></Profile>
-      </div>
+    <header className="flex p-2 fixed w-[100%] bg-background z-50 border-b-[1px] items-center justify-between border-input px-4 sm:px-8 md:px-16">
+      <Profile></Profile>
 
       <div className="flex gap-4">
         <Input type="text" placeholder="Users names" />
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,11 @@
 "use client";
 
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
-import { ChevronDown, LogOut } from "lucide-react";
+import { ChevronDown, Divide, LogOut } from "lucide-react";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Avatar } from "./ui/avatar";
-import { Button } from "./ui/button";
 
 import { useRouter } from "next/navigation";
-
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@radix-ui/react-popover";
 
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
@@ -39,58 +32,17 @@ export default function Profile() {
   }, [userAuth]);
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Avatar className="cursor-pointer">
-          <AvatarImage
-            src="https://github.com/Antonio-Gabriel.png"
-            alt="@antonio"
-          />
-          <AvatarFallback>
-            {getInitials(userAuth?.name || "NOT FUND")}
-          </AvatarFallback>
-        </Avatar>
-      </PopoverTrigger>
-
-      <PopoverContent className="w-80">
-        <Card className="relative overflow-hidden rounded-lg">
-          <CardHeader
-            className="h-[150px] w-full bg-cover bg-center blur-md"
-            style={{
-              backgroundImage: `url('https://github.com/Antonio-Gabriel.png')`,
-            }}
-          ></CardHeader>
-
-          <div className="absolute top-[100px] left-1/2 transform -translate-x-1/2">
-            <Avatar className="w-24 h-24 border-4 border-white rounded-full">
-              <AvatarImage
-                src="https://github.com/Antonio-Gabriel.png"
-                alt="@shadcn"
-              />
-              <AvatarFallback>
-                {getInitials(userAuth?.name || "NOT FUND")}
-              </AvatarFallback>
-            </Avatar>
-          </div>
-
-          <CardContent className="mt-[80px] text-center">
-            <div className="my-[10px]">
-              <p className="font-semibold">Adão Angelo João</p>
-              <p className="text-gray-500 text-[14px]">@adaobegginer</p>
-            </div>
-          </CardContent>
-
-          <CardFooter>
-            <Button
-              onClick={handleLogout}
-              variant="destructive"
-              className="w-[100%] flex justify-start gap-2 text-left"
-            >
-              <LogOut /> <p>Log out</p>
-            </Button>
-          </CardFooter>
-        </Card>
-      </PopoverContent>
-    </Popover>
+    <div className="flex items-center gap-2">
+      <Avatar>
+        <AvatarImage
+          src={userAuth?.avatar || "https://github.com/Adao-Angelo.png"}
+        ></AvatarImage>
+        <AvatarFallback>{getInitials(userAuth?.name ?? "User")}</AvatarFallback>
+      </Avatar>
+      <div>
+        <h2 className="text-[14px] font-bold">{userAuth?.name}</h2>
+        <p className="text-[10px]">{userAuth?.email}</p>
+      </div>
+    </div>
   );
 }
