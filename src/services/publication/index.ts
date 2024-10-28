826656287af64aff1a02c38aef94fe1dfb459f4d
Adao-Angelo <adaobegginer@gmail.com>
2024-10-28 16:25:29 +0100

    creata publication type

---
+++ src/services/publication/index.ts
@@ -0,0 +1,45 @@
+import { http } from "@/common/https";
+
+import { PublicationType } from "@/types";
+
+export namespace PublicationService {
+  export async function createPublication(data: PublicationType) {
+    try {
+      const response = await http.post("/publications", data);
+      return response.data;
+    } catch (error) {
+      console.error("Error creating a Publication: ", error);
+      throw error;
+    }
+  }
+
+  export async function fetchPublications() {
+    try {
+      const response = await http.get("/publications");
+      console.log(`Records: ${response.data}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching publications: ", error);
+      throw error;
+    }
+  }
+
+  export async function deletePublication(id?: string) {
+    try {
+      await http.delete(`/publications/${id}`);
+    } catch (error) {
+      console.error("Error deleting publications: ", error);
+      throw error;
+    }
+  }
+
+  export async function updatePublication(id: string, data: PublicationType) {
+    try {
+      const response = await http.patch(`/publications/${id}`, data);
+      return response.data;
+    } catch (error) {
+      console.error("Error updating Publication: ", error);
+      throw error;
+    }
+  }
+}
