2dbe2d7f5749d28b2cffe8a37f4523119aa4702f
Adao-Angelo <adaobegginer@gmail.com>
2024-11-04 22:08:32 +0100

    create page

--- src/common/http.ts
+++ src/common/http.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-// const BaseUrl =
-//   process.env.BASE_URL || "https://no-offense-backend.onrender.com";
+const BaseUrl =
+  process.env.BASE_URL || "https://no-offense-backend.onrender.com";
 
-const BaseUrl = process.env.BASE_URL || "http://localhost:2000";
+// const BaseUrl = process.env.BASE_URL || "http://localhost:2000";
 const accessToken = Cookies.get("token");
 
 const http = axios.create({
